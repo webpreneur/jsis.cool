9d73af093f46fd9b3239bfc64fad6e7c4f0e43a2
Gulyás Zsolt <zsolt.gulyas@moresimp.com>
2022-12-13 15:05:50 +0100

    Add meta params and navigation to app component.
    Clear boilerplate.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
+  selector: 'jic-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'jsIsCool';
+export class AppComponent implements OnInit {
+
+  private _title: string;
+
+  constructor(
+    private _titleService: Title,
+    private _metaTagService: Meta,
+  ) {
+    this._title = 'JsIsCool'
+  }
+
+  ngOnInit(): void {
+    this._titleService.setTitle(this._title);
+    this._metaTagService.addTags([
+      { name: 'description', content: 'JS is Cool homepage' },
+      { name: 'author', content: 'webpreneur' },
+      { name: 'keywords', content: 'Angular, TypeScript, JavaScript, SASS, CSS' },
+    ]);
+  }
+
 }
