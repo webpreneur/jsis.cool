df2381bc1a4e5f79400103fe7dd524c00144686f
Gulyás Zsolt <zsolt.gulyas@moresimp.com>
2022-12-13 15:03:15 +0100

    Add Components: Post, Not Found, Home.

---
+++ frontend/src/app/modules/blog/components/post/post.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'jic-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+
+  @Input() public title: string;
+  @Input() public created: Date;
+
+  public title$: Observable<string>;
+
+  constructor(
+    private _route: ActivatedRoute,
+  ) {
+    this.title = '{$title}';
+    this.created = new Date();
+    this.title$ = of('${title}');
+  }
+
+  ngOnInit(): void {
+      this.title$ = this._route.paramMap.pipe(map((params) => params.get('id')!))
+  }
+}
