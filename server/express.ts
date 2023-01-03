cad29250c3134a198bfbb5093226e9ac88a6adb3
Gulyás Zsolt <zsolt.gulyas@moresimp.com>
2023-01-03 16:55:19 +0100

    Add npm express with types.

---
+++ server/express.ts
@@ -0,0 +1,12 @@
+// @deno-types="npm:@types/express@4.17.15"
+import express from "npm:express@4.18.2";
+
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000);
+console.log("listening on http://localhost:3000/");
