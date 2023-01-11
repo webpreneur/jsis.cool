2a3efef19a9f24994d38aaa3e4cfbea144f5c2ee
Gulyás Zsolt <zsolt.gulyas@moresimp.com>
2023-01-11 13:02:19 +0100

    Init Angular Client app.

---
+++ openai/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'openai-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'openai';
+}
